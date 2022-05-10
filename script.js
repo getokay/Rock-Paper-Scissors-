f9bf4615a5e83eb5a6465465cc2af50a0296eb31
getokay <tomeix13@gmail.com>
2022-05-10 18:21:54 -0300

    Added how to decide the winner of a round

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 function computerPlay() {
   const play = Math.floor(Math.random() * 3 + 1);
   if (play == 1) {
-    return "Rock";
+    return "rock";
   } else if (play == 2) {
-    return "Paper";
+    return "paper";
   } else {
-    return "Scissors";
+    return "scissors";
+  }
+}
+function winner(player, computer) {
+  player = player.toLowerCase();
+  if (player == computer) {
+    return "deuce";
+  } else if (
+    (player == "rock" && computer == "scissors") ||
+    (player == "paper" && computer == "rock") ||
+    (player == "scissors" && computer == "paper")
+  ) {
+    return "playerWin";
+  } else {
+    return "computerWin";
   }
 }
